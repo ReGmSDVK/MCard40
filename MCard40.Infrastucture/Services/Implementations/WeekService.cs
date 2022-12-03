24cf33fff0f6587e07ec122fa167876712ebcf91
Cholponai <cholponay112@gmail.com>
2022-12-03 14:48:58 +0600

    Create: CardPageService

--- MCard40.Infrastucture/Services/Implementations/WeekService.cs
+++ MCard40.Infrastucture/Services/Implementations/WeekService.cs
@@ -1,13 +1,54 @@
 ﻿using MCard40.Infrastucture.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MCard40.Model.Entity;
+using MCard40.Model.Interfaces;
+
 
 namespace MCard40.Infrastucture.Services.Implementations
 {
     public class WeekService : IWeekService
     {
+        private readonly IRepository<Week, int> _repository;
+
+        public WeekService(IRepository<Week, int> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(Week week)
+        {
+            _repository.Create(week);
+        }
+
+        public Week Delete(int id)
+        {
+            var week = _repository.DeleteById(id);
+            if (week == null)
+            {
+                return null;
+            }
+
+            return week;
+        }
+
+        public Week GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Week GetWeekDetails(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            var week = _repository.ReadById(id.Value);
+            return week;
+        }
+
+        public Week Update(int id, Week patient)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MCard40.Infrastucture/Services/Interfaces/IWeekService.cs
+++ MCard40.Infrastucture/Services/Interfaces/IWeekService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MCard40.Model.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace MCard40.Infrastucture.Services.Interfaces
 {
     public interface IWeekService
     {
+        void Add(Week week);
+        Week GetById(int? id);
+        Week Update(int id, Week week);
+        Week GetWeekDetails(int? id);
+        Week Delete(int id);
     }
 }
