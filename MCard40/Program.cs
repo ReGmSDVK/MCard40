72394150af86802bc5315db353235d42c21e1c86
Cholponai <cholponay112@gmail.com>
2022-12-07 15:43:44 +0600

    Added front to page Login

--- MCard40/Program.cs
+++ MCard40/Program.cs
@@ -52,7 +52,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=TypesDoctor}/{action=Price}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
