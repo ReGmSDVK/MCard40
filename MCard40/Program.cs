83d15f6c5d03b423fb1d7ffb4cc5ab671df4e817
Cholponai <cholponay112@gmail.com>
2022-12-11 00:59:04 +0600

    blog about reviews

--- MCard40/Program.cs
+++ MCard40/Program.cs
@@ -55,7 +55,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Patient}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
